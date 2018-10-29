e2c363d67727b5c84f81cf322551d3ee671ededa
hujinqiang <854024269@qq.com>
2018-10-29 14:36:31 +0800

    新代码提交

---
+++ app/src/main/java/com/tocel/kotlintest/DSLTest.kt
@@ -0,0 +1,14 @@
+package com.tocel.kotlintest
+
+fun main(args: Array<String>) {
+    val appendExcl:StringBuilder.() -> Unit = {append("!")}
+
+
+    val stringBuilder = StringBuilder("Hi")
+    stringBuilder.appendExcl()
+    println(stringBuilder)
+
+    println(buildString { append("abcdefg") })
+}
+
+fun buildString(builderAction: StringBuilder.() -> Unit):String = StringBuilder().apply(builderAction).toString()
